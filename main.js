f950e54e466a8811ac21af8c90a5249ab6f1904b
Diego Alfonso <dlalfonso57@hotmail.com>
2023-01-30 21:14:19 -0500

    modificando el menu

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
 const navbarEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
+const menuHamIcon = document.querySelector(".menu");
+const MobileMenu = document.querySelector(".mobile-menu");
+
 navbarEmail.addEventListener("click", toggleDesktopMenu);
+menuHamIcon.addEventListener("click", toggleMobileMenu);
 
 function toggleDesktopMenu() {
   desktopMenu.classList.toggle("inactive");
+}
+
+function toggleMobileMenu() {
+  MobileMenu.classList.toggle("inactive");
 }
