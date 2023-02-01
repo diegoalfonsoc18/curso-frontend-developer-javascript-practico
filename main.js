a0a410de452abeb4db09d4ef55dd2c0849fb70a5
Diego Alfonso <dlalfonso57@hotmail.com>
2023-02-01 12:01:24 -0500

    menu repair

--- main.js
+++ main.js
@@ -1,15 +1,38 @@
 const navbarEmail = document.querySelector(".navbar-email");
+const menuCarritoIcon = document.querySelector(".navbar-shopping-cart");
 const desktopMenu = document.querySelector(".desktop-menu");
 const menuHamIcon = document.querySelector(".menu");
-const MobileMenu = document.querySelector(".mobile-menu");
+const mobileMenu = document.querySelector(".mobile-menu");
+const aside = document.querySelector(".product-detail");
 
 navbarEmail.addEventListener("click", toggleDesktopMenu);
 menuHamIcon.addEventListener("click", toggleMobileMenu);
+menuCarritoIcon.addEventListener("click", toggleCarritoAside);
 
 function toggleDesktopMenu() {
+  const isAsideMenuClosed = aside.classList.contains("inactive");
+
+  if (!isAsideMenuClosed) {
+    aside.classList.add("inactive");
+  }
   desktopMenu.classList.toggle("inactive");
 }
 
 function toggleMobileMenu() {
-  MobileMenu.classList.toggle("inactive");
+  const isAsideMenuClosed = aside.classList.contains("inactive");
+
+  if (!isAsideMenuClosed) {
+    aside.classList.add("inactive");
+  }
+
+  mobileMenu.classList.toggle("inactive");
+}
+
+function toggleCarritoAside() {
+  const isMobileMenuClosed = mobileMenu.classList.contains("inactive");
+
+  if (!isMobileMenuClosed) {
+    mobileMenu.classList.add("inactive");
+  }
+  aside.classList.toggle("inactive");
 }
